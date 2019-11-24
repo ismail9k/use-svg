cad6172438db489f88608e4a0313396dd676fbcb
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-11-24 14:23:29 +0300

    feat: add app icon component

---
+++ src/AppIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg class="icon" style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path :d="path" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'AppIcon',
+  props: {
+    path: String,
+  },
+};
+</script>
