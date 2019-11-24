e5c97b0a8bd5353f4c2cc660f383c0877ba6b572
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-11-24 14:38:44 +0300

    feat: add styling

--- src/AppIcon.vue
+++ src/AppIcon.vue
@@ -1,8 +1,8 @@
 <template>
   <svg
-    class="icon"
-    style="width:24px;height:24px"
     viewBox="0 0 24 24"
+    class="icon"
+    :class="styleClasses"
   >
     <path :d="path" />
   </svg>
@@ -15,11 +15,65 @@ export default {
   name: "AppIcon",
   props: {
     name: String,
+    size: String,
+    color: String,
   },
   computed: {
     path() {
       return icons[this.name];
     },
+    styleClasses() {
+      return [this.size, this.color].map(val => val && `is-${val}`);
+    },
   },
 };
 </script>
+
+<style>
+/* default */
+.icon {
+  width: 1.2em;
+  height: 1.2em;
+  fill: currentColor;
+}
+
+/* sizes */
+.icon.is-large {
+  width: 2em;
+  height: 2em;
+}
+.icon.is-medium {
+  width: 1.6em;
+  height: 1.6em;
+}
+.icon.is-normal {
+  width: 1.2em;
+  height: 1.2em;
+}
+.icon.is-small {
+  width: 1em;
+  height: 1em;
+}
+.icon.is-tiny {
+  width: 0.8em;
+  height: 0.8em;
+}
+
+/* colors */
+.icon.is-primary,
+.icon.is-blue {
+  fill: #2196F3
+}
+.icon.is-success,
+.icon.is-green {
+  fill: #4CAF50
+}
+.icon.is-danger,
+.icon.is-red  {
+  fill: #F44336
+}
+.icon.is-warning,
+.icon.is-orange {
+  fill: #FF9800
+}
+</style>
